c1f1b95597e1425c4d1c51af7745f95108ed867f
yutongselinaxiao <yutongselinaxiao@gmail.com>
2026-04-14 21:25:52 -0700

    added run scripts for online gpt2 training

---
+++ GPT2_new_version/Medium_XL_GPT/config/train_gpt2_medium_online_linearized.py
@@ -0,0 +1,43 @@
+# GPT-2 Medium + Online Convex Balancing (linearized ADMM)
+batch_size = 16
+block_size = 1024
+gradient_accumulation_steps = 1
+
+n_layer = 24
+n_head = 16
+n_embd = 1024
+dropout = 0.0
+bias = False
+
+max_iters = 50000
+lr_decay_iters = 100000
+
+# eval stuff
+eval_interval = 100
+eval_iters = 200
+log_interval = 10
+ckpt_interval = 1000
+
+# optimizer
+algorithm = 'sisa'
+learning_rate = 0.02
+embed_learning_rate = 0.0036
+muon_learning_rate = 0.02
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+epsilon = 1e-6
+grad_clip = 1.0
+decay_lr = True
+min_lr = 6e-5
+
+# ADMM mode
+admm_mode = 'linearized'
+
+# wandb
+use_wandb = True
+wandb_project = 'gpt2-sisa-online'
+
+comment = 'gpt2_medium_online_linearized'
+save_dir = '/dataMeR2/yutong/sisa_gpt2/log_gpt2/' + comment
+out_dir = '/dataMeR2/yutong/sisa_gpt2/out_gpt2/' + comment
---
+++ GPT2_new_version/Medium_XL_GPT/run_online_exact.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nproc_per_node=4 --master_port 12346 train_gpt_sisa_lower_no_2ndgradient_online.py config/train_gpt2_medium_online_exact.py
